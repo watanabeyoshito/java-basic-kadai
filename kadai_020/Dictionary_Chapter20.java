8b2fbb4a8cde172b818ab05726e1df89b6d71be8
watanabeyoshito <yoshito1003pd@gmail.com>
2024-07-29 18:17:02 +0900

    追加

---
+++ kadai_020/Dictionary_Chapter20.java
@@ -0,0 +1,35 @@
+package kadai_020;
+
+import java.util.HashMap;
+
+public class Dictionary_Chapter20 {
+
+    private HashMap<String, String> dictionary;
+    
+    public Dictionary_Chapter20() {
+        dictionary = new HashMap<String, String>();
+        
+        dictionary.put("apple", "りんご");
+        dictionary.put("peach", "桃");
+        dictionary.put("banana", "バナナ");
+        dictionary.put("lemon", "レモン");
+        dictionary.put("pear", "梨");
+        dictionary.put("kiwi", "キウイ");
+        dictionary.put("strawberry", "いちご");
+        dictionary.put("grape", "ぶどう");
+        dictionary.put("muscat", "マスカット");
+        dictionary.put("cherry", "さくらんぼ");
+    }
+
+    public void search(String[] words) {
+        for (String word : words) {
+            if (dictionary.containsKey(word)) {
+                System.out.println(word + "の意味は" + dictionary.get(word));
+            } else {
+                System.out.println(word + "は辞書に存在しません");
+            }
+        }
+    }
+}
+
+
